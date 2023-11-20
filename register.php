547aac001acb4e547d8a80da0433dda66749c728
YASHASVI PARASHAR <yashasviparashar28@gmail.com>
2023-11-20 13:24:18 +0530

    register.php

---
+++ register.php
@@ -0,0 +1,30 @@
+<?php
+// Database connection parameters
+$conn = mysqli_connect('localhost', 'root', '', 'shopcity');
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Process registration form
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $address = $_POST["address"];
+    $phone = $_POST["phone"];
+    $email = $_POST["email"];
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+    // Insert user data into the database
+    $sql = "INSERT INTO users (name, address, phone, email, password) VALUES ('$name', '$address', '$phone', '$email', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Registration successful!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+// Close connection
+$conn->close();
+?>
